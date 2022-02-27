e7dbf719581c3637aea9fbb93bd521c918eebc05
Zek Wazaowsky <87409484+Zekkes@users.noreply.github.com>
2022-02-27 19:45:28 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
+
+var to = document.getElementById("to");
+var subject = document.getElementById("subject");
+var body = document.getElementById("body");
+
+function Send(){
+    
+    Email.send({
+      SecureToken: "61e09161-b4cc-45fe-9146-007cbbee33ec",
+      From: "zek@666.id",
+      To: to.value,
+      Subject: subject.value,
+      Body: body.value,
+    }).then(
+      error => alert(error),
+    
+    )
+}
